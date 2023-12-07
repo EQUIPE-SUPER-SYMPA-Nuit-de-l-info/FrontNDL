ed04c50a8684fc3f66f7ab0299035ad19a36bbec
CorentinAT <corentin.al2004@gmail.com>
2023-12-07 03:32:01 +0100

    strucute page vrai faux

--- src/routes/quizz/+page.svelte
+++ src/routes/quizz/+page.svelte
@@ -1,1 +1,26 @@
-<h1>Big Quizz</h1>
+<style>
+    @import './quizz.css';
+</style>
+
+<main>
+    <div id="vraifaux">
+        <div id="faux">
+            FAUSSE INFO
+        </div>
+        <div id="vrai">
+            VRAIE INFO
+        </div>
+    </div>
+    <div id="tweet">
+        <div id="contenu">
+            <div id="auteur">
+                <img src="https://thumbs.dreamstime.com/b/profil-vectoriel-avatar-par-d%C3%A9faut-utilisateur-179376714.jpg" alt="avatar"/>
+                <p id="nom-auteur">Auteur</p>
+            </div>
+            <p id="affirmation">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500.</p>
+        </div>
+        <div id="source">
+            <p>source</p>
+        </div>
+    </div>
+</main>
