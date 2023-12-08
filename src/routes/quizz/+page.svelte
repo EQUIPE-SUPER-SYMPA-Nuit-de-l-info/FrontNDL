9269bb30e154677906b149799b956386b540087c
Risson <aime.risson.1@gmail.com>
2023-12-08 01:15:36 +0100

    tinder

--- src/routes/quizz/+page.svelte
+++ src/routes/quizz/+page.svelte
@@ -1,89 +1,132 @@
 <script>
-    import { onMount } from 'svelte';
+  import { onMount } from "svelte";
 
-    let tweetSelect = null;
-    let divFaux;
-    let divVrai;
-    let divTweet;
-    let mouseX;
-    let mouseY;
+  let tweetSelect = null;
+  let divFaux;
+  let divVrai;
+  let divTweet;
+  let mouseX;
+  let mouseY;
 
-    onMount(() => {
-        divFaux = document.getElementById("faux");
-        divVrai = document.getElementById("vrai");
-        divTweet = document.getElementById("tweet")
-    });
+  onMount(() => {
+    divFaux = document.getElementById("faux");
+    divVrai = document.getElementById("vrai");
+    divTweet = document.getElementById("tweet");
+  });
 
-    $:if(tweetSelect===true && mouseX>window.innerWidth/2) {
-        lightVrai();
-        darkFaux();
-    }
-    $:if(tweetSelect===false) {
-        darkVrai();
-    }
-    $:if(tweetSelect===true && mouseX<window.innerWidth/2) {
-        lightFaux();
-        darkVrai();
-    }
-    $:if(tweetSelect===false) {
-        darkFaux();
-    }
+  $: if (tweetSelect === true && mouseX > window.innerWidth / 2) {
+    lightVrai();
+    darkFaux();
+  }
+  $: if (tweetSelect === false) {
+    darkVrai();
+  }
+  $: if (tweetSelect === true && mouseX < window.innerWidth / 2) {
+    lightFaux();
+    darkVrai();
+  }
+  $: if (tweetSelect === false) {
+    darkFaux();
+  }
 
-    function onMouseMove(e) {
-        if(tweetSelect) {
-            mouseX = e.clientX;
-            mouseY = e.clientY;
-            divTweet.style.marginLeft = `${mouseX-(window.innerWidth/2)}px`;
-            divTweet.style.marginTop = `${mouseY-(window.innerHeight/2)}px`;
-        }
+  function onMouseMove(e) {
+    if (tweetSelect) {
+      mouseX = e.clientX;
+      mouseY = e.clientY;
+      divTweet.style.marginLeft = `${mouseX - window.innerWidth / 2}px`;
+      divTweet.style.marginTop = `${mouseY - window.innerHeight / 2}px`;
     }
-    function onMouseDown() {
-        tweetSelect = true;
-    }
-    function onMouseUp() {
-        tweetSelect = false;
-    }
-    function lightFaux() {
-        divFaux.classList.remove('dark-faux');
-        divFaux.classList.add('light-faux');
-    }
-    function darkFaux() {
-        divFaux.classList.remove('light-faux');
-        divFaux.classList.add('dark-faux');
-    }
-    function lightVrai() {
-        divVrai.classList.remove('dark-vrai');
-        divVrai.classList.add('light-vrai');
-    }
-    function darkVrai() {
-        divVrai.classList.remove('light-vrai');
-        divVrai.classList.add('dark-vrai');
-    } 
+  }
+  function onMouseDown() {
+    tweetSelect = true;
+  }
+  function onMouseUp() {
+    tweetSelect = false;
+  }
+  function lightFaux() {
+    divFaux.classList.remove("dark-faux");
+    divFaux.classList.add("light-faux");
+  }
+  function darkFaux() {
+    divFaux.classList.remove("light-faux");
+    divFaux.classList.add("dark-faux");
+  }
+  function lightVrai() {
+    divVrai.classList.remove("dark-vrai");
+    divVrai.classList.add("light-vrai");
+  }
+  function darkVrai() {
+    divVrai.classList.remove("light-vrai");
+    divVrai.classList.add("dark-vrai");
+  }
 </script>
 
 <div id="main" on:mousemove={onMouseMove} role="button" tabindex="0">
-    <div id="vraifaux">
-        <div id="faux" class="dark-faux" role="button" tabindex="0">
-            FAUSSE INFO
-        </div>
-        <div id="vrai" class="dark-vrai" role="button" tabindex="0">
-            VRAIE INFO
-        </div>
+  <div id="vraifaux">
+    <div id="faux" class="dark-faux" role="button" tabindex="0">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="w-4 h-4"
+      >
+        <path
+          d="M11 2a2 2 0 0 0-2 2v5H4a2 2 0 0 0-2 2v2c0 1.1.9 2 2 2h5v5c0 1.1.9 2 2 2h2a2 2 0 0 0 2-2v-5h5a2 2 0 0 0 2-2v-2a2 2 0 0 0-2-2h-5V4a2 2 0 0 0-2-2h-2z"
+        ></path>
+      </svg>
     </div>
-    <div id="tweet" on:mousedown={onMouseDown} on:mouseup={onMouseUp} role="button" tabindex="0">
-        <div id="contenu">
-            <div id="auteur">
-                <img src="https://thumbs.dreamstime.com/b/profil-vectoriel-avatar-par-d%C3%A9faut-utilisateur-179376714.jpg" alt="avatar"/>
-                <p id="nom-auteur">Auteur</p>
-            </div>
-            <p id="affirmation">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500.</p>
-        </div>
-        <div id="source">
-            <p>source</p>
-        </div>
+    <div id="vrai" class="dark-vrai" role="button" tabindex="0">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="w-4 h-4"
+      >
+        <path
+          d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"
+        ></path>
+      </svg>
     </div>
+  </div>
+  <div
+    id="tweet"
+    on:mousedown={onMouseDown}
+    on:mouseup={onMouseUp}
+    role="button"
+    tabindex="0"
+  >
+    <div id="contenu">
+      <div id="auteur">
+        <img
+          src="https://thumbs.dreamstime.com/b/profil-vectoriel-avatar-par-d%C3%A9faut-utilisateur-179376714.jpg"
+          alt="avatar"
+        />
+        <p id="nom-auteur">Auteur</p>
+      </div>
+      <p id="affirmation">
+        Le Lorem Ipsum est simplement du faux texte employé dans la composition
+        et la mise en page avant impression. Le Lorem Ipsum est le faux texte
+        standard de l'imprimerie depuis les années 1500.
+      </p>
+    </div>
+    <div id="source">
+      <p>source</p>
+    </div>
+  </div>
 </div>
 
 <style>
-    @import './quizz.css';
+  @import "./quizz.css";
 </style>
