6aa5580509d97ae25f7cf40d24f27ef5ff511fca
MarcLeMarco <marc.sabattier@etu.univ-lyon1.fr>
2023-12-08 05:35:06 +0100

    aaa

---
+++ src/routes/debunk/+page.svelte
@@ -0,0 +1,76 @@
+<main>
+    
+    <div id="body">
+        <p id="intro">
+            Internet à pour avantage de permettre à n'importe qui d'exprimer son avis et de se faire entendre par qui veut le lire. Mais cette qualité est aussi parfois son plus grand défaut. En effet, la toile regorge d'informations erronées, d'avis désastreux et déconnectés de la réalité, qui vont parfois jusqu'au complotisme. Nous allons donc tenter d'infirmer les arguments les plus communs des climatosceptiques.         </p>
+        <div id="contenu">
+            <div>
+                <h2>"L'hiver, on bat ces dernières années des records de froid : il est où le "réchauffement climatique" ?"</h2>
+                <p>
+                    Le climat ne désigne pas simplement la température à un instant t : il n'est pas possible de tirer des conclusions sur son état à partir de données sur une journée, un mois ou un an. Les variations du climat s'évaluent en fait sur des périodes d'au moins 30 ans. Et sur les 30 dernières années, on voit clairement une évolution du climat à l'échelle mondiale, plus précisément un réchauffement de ce dernier, ce pourquoi on parle de réchauffement climatique.                </p>
+            </div>
+            <div>
+                <h2>"De toute façon, le climat fait des cycles et change en permanence depuis toujours..."</h2>
+                <p>
+                    Il est vrai que les facteurs naturels font évoluer le climat depuis la naissance de notre planète. Mais on constate actuellement un changement beaucoup plus rapide que le fameux cycle naturel du climat. D'ailleurs, la science moderne est assez développée pour faire des simulations de l'évolution du climat avec et sans l'activité humaine, et ces simulations montrent clairement que le changement climatique actuel est dû à notre influence.                </p>
+            </div>
+            <div>
+                <h2>"On parle de changement climatique dès qu'il fait un peu chaud, mais on a toujours connu des canicules !"</h2>
+                <p>
+                    Certes, nous avons toujours connu des canicules. Mais justement, le changement climatique fait que ces dernières sont plus fréquentes et plus violentes. On avait droit à environ une canicule tous les 5 ans avant 1989, mais on en compte en moyenne près de 2 chaque année depuis 2010.                </p>
+            </div>
+            <div>
+                <h2>"Bla bla bla changement climatique, 1 ou 2 degrés de plus c'est rien de toute façon."</h2>
+                <p>
+                    En fait, 1 ou 2 degrés à l'échelle du monde, c'est énorme. Penser que ce n'est rien, c'est confondre climat et météo. On parle ici d'une augmentation globale des températures dans le monde entier, et les conséquence de celle-ci sont déjà visibles et très documentées.                 </p>
+            </div>
+        </div>
+    </div>
+</main>
+<style>
+    main {
+        height: 100vh;
+        font-size: large;
+    }
+
+    #intro {
+        font-size: larger;
+    }
+
+    #body {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 3rem
+    }
+
+    #header {
+        display: flex;
+        align-items: center;
+        background-color: #6c91c2;
+        border-bottom: medium solid #34623f;
+    }
+
+    #titrePage {
+        font-size: x-large;
+        font-weight: bold;
+    }
+
+    #titrePage a {
+        text-decoration: none;
+    }
+
+    #logo {
+        width: 12rem
+    }
+
+    #contenu div {
+        border: thin solid #34623f;
+        padding: 1rem;
+        border-radius: 1rem;
+        box-shadow: inset 0 0 2rem #aaabbc;
+        margin: 1rem
+    }
+
+
+</style>
