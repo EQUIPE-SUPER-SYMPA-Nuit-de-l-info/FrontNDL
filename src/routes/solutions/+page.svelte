653ac241ce9b9c1b44a8a6b760a050d7ffcb3d4f
MarcLeMarco <marc.sabattier@etu.univ-lyon1.fr>
2023-12-08 05:41:15 +0100

    supersympa

---
+++ src/routes/solutions/+page.svelte
@@ -0,0 +1,65 @@
+<main>
+    
+    <div id="body">
+        <p id="intro">
+            Le réchauffement climatique est un phénomène qui met en danger l'Humanité toute entière. Il est donc impératif de réduire drastiquement les émissions de gaz à effet de serre dans le monde entier au plus vite. Le <a href="https://www.ipcc.ch/languages-2/francais/">GIEC</a> donne chaque année dans son rapport des objectifs de réduction de ces émissions, mais la question des mesures nécessaires à la réalisation de ces objectifs reste entière. 
+            Les décideurs du monde entier ont en effet une fâcheuse tendance à en faire beaucoup moins que recommandé, invoquant des raisons assez peu convaincantes : la santé de l'économie prend souvent trop de poids dans la balance. 
+            Mais si ceux qui sont censés le faire ne proposent pas de vraies solutions au désastre qui nous attend, comment assurer le futur de l'Homme, et surtout de la planète qui l'héberge ?        </p>
+        <div id="contenu">
+            <p>
+                La majeure partie des émissions de gaz à effet de serre sont émis directement ou non lors de la production de biens par les individus. Que ce soit leur production en elle même, leur transport, ou même leur utilisation, les biens que nous produisons et utilisons au quotidien, qui font notre confort de vie, sont aussi la cause principale de notre potentielle extinction. Il semble alors impératif de sortir de ce modèle de société, centré autour de l'idée de produire plus pour gagner plus et vivre mieux. 
+                Mais l'idée de perdre une partie de notre confort de vie fait peur. Sortir du modèle productiviste, c'est choisir d'arrêter de fabriquer une immense partie de ce que l'on consomme. Il semble alors important de rappeler que si notre mode de vie occidental serait en effet très impacté par de tels changements de société, ce n'est pas le cas de tous nos confrères humains. Notre mode de vie occidental doit en effet son confort à l'exploitation massive des populations les plus pauvres, qui n'ont le droit qu'à une infime fraction du dit confort. 
+                Sortir du modèle productiviste, en plus de permettre à la terre de continuer d'abriter la vie, serait donc une excellente opportunité pour garantir une existence digne à tout le monde. Les causes sociales et écologiques se rejoignent, et la lutte pour un monde plus juste pour tous va alors de pair avec celle pour un monde plus propre.
+                <a href="https://www.statistiques.developpement-durable.gouv.fr/edition-numerique/chiffres-cles-du-climat-2022/7-repartition-sectorielle-des-emissions-de">Sources</a>
+            </p>
+        </div>
+    </div>
+</main>
+<style>
+
+    * {
+        text-align: justify;
+    }
+
+    main {
+        height: 100vh;
+        font-size: large;
+    }
+
+    #body {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 3rem
+    }
+
+    #header {
+        display: flex;
+        align-items: center;
+        background-color: #6c91c2;
+        border-bottom: medium solid #34623f;
+    }
+
+    #titrePage {
+        font-size: x-large;
+        font-weight: bold;
+    }
+
+    #titrePage a {
+        text-decoration: none;
+    }
+
+    #logo {
+        width: 12rem
+    }
+
+    #contenu p {
+        border: thin solid #34623f;
+        padding: 1rem;
+        border-radius: 1rem;
+        box-shadow: inset 0 0 2rem #aaabbc;
+        font-size: larger;
+    }
+
+
+</style>
